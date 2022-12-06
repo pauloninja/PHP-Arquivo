adbb1976a028c3d742841bbadee663fa0a604620
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2022-12-06 14:36:58 -0300

    atualizar

--- avancado/public/unidade_04/listagem.php
+++ avancado/public/unidade_04/listagem.php
@@ -1,5 +1,17 @@
 <?php require_once("../../conexao.php/conexao.php"); ?>
 
+<?php
+
+    // Consulta ao banco de dados - produtos
+   $produtos = "SELECT produtoID, nomeproduto, tempoentrega, precounitario ";
+   $produtos .= " FROM produtos ";
+   $resultado = mysqli_query($conecta,$produtos);//conecta arquivo de conexao
+   // if(!$resultado){
+   //     die("Falha na consulta ao banco");
+   // }
+
+?>
+
 <!doctype html>
 <html>
     <head>
@@ -9,8 +21,7 @@
         
         <!-- estilo -->
         <link href="_css/estilo.css" rel="stylesheet">
-        <link  href="_css/produtos.css" rel="stylesheet">
-        <link  href="_css/produtos_pesquisa.css" rel="stylesheet">
+      
 
         
     </head>
@@ -20,6 +31,20 @@
         <?php include_once("../_incluir/funcoes.php"); ?>
         
         <main>  
+            <?php 
+                while($linha = mysqli_fetch_assoc($resultado)){
+
+                
+            ?>
+            <ul>
+                <li><?php echo $linha["nomeproduto"]?></li>
+                <li><?php echo $linha["tempoentrega"]?></li>
+                <li><?php echo $linha["precounitario"]?></li>
+            </ul>
+            <?php
+                }
+
+            ?>
             
         </main>
 
