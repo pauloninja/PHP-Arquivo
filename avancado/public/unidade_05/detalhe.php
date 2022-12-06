93f704a2df25e028370619362c81cb2d9e66a714
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2022-12-06 16:54:11 -0300

    atualizar

--- avancado/public/unidade_05/detalhe.php
+++ avancado/public/unidade_05/detalhe.php
@@ -6,8 +6,27 @@
         Header("Location: listagem.php");
     }
 
-    //Consulta
+    //Consulta ao banco
+    $consulta = "SELECT * ";
+    $consulta .= "FROM produtoS ";
+    $consulta .= "WHERE produtoID = {$produtoID} ";
+    $detalhe = mysqli_query($conecta,$consulta);
 
+    if(!$detalhe ){
+        die("Falha no banco de dados");
+    } else{
+        $dados_detalhe = mysqli_fetch_assoc($detalhe);
+        $produtoID = $dados_detalhe["produtoID"];
+        $nomeproduto = $dados_detalhe["nomeproduto"];
+        $descricao = $dados_detalhe["descricao"];
+        $codigobarra = $dados_detalhe["codigobarra"];
+        $tempoentrega = $dados_detalhe["tempoentrega"];
+        $precorevenda = $dados_detalhe["precorevenda"];
+        $precounitario = $dados_detalhe["precounitario"];
+        $estoque = $dados_detalhe["estoque"];
+        $imagemgrande = $dados_detalhe["imagemgrande"];
+
+    }
 ?>
 
 <!doctype html>
@@ -19,6 +38,8 @@
         
         <!-- estilo -->
         <link href="_css/estilo.css" rel="stylesheet">
+        <link href="_css/produto_detalhe.css" rel="stylesheet">
+
     </head>
 
     <body>
@@ -26,7 +47,19 @@
         <?php include_once("../_incluir/funcoes.php"); ?> 
         
         <main>  
-            
+           <div id="detalhe_produto">
+            <ul>
+                <li class="imagem"><img src="<?php echo $imagemgrande ?>" ></li>
+                <li><h2><?php echo $nomeproduto ?></h2></li>
+                <li><?php echo $descricao ?></li>
+                <li>Código de barra: <?php echo $codigobarra ?></li>
+                <li>Tempo de entrega: <?php echo $tempoentrega ?> dias.</li>
+                <li>Preço Revenda: <?php echo real_format($precorevenda) ?></li>
+                <li>Preço Unitario: <?php echo real_format($precounitario) ?></li>
+                <li>Estoque: <?php echo $estoque ?></li>
+
+            </ul>
+           </div>
         </main>
 
         <?php include_once("../_incluir/rodape.php"); ?>
